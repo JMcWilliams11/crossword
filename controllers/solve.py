ae9bafd75102e711b621ff070d7829de8ec77c2e
Jack McWilliams <jmcwill@umich.edu>
2016-10-08 10:40:20 -0400

    JM - using beautiful soup 4 to parse html

--- controllers/solve.py
+++ controllers/solve.py
@@ -1,9 +1,21 @@
 from flask import *
+import requests
+import bs4
 
 solve = Blueprint('solve', __name__, template_folder='templates')
 
 @solve.route('/solve', methods=['POST'])
 def solve_route():
+    #puzzleUrl = new york times crossword puzzle url
+    puzzleUrl = request.form['url']
+    req = requests.get(puzzleUrl)
+    if req.status_code == requests.codes.ok:
+
+    else :
+        goodUrl = False
+        options = {
+            'goodUrl': goodUrl
+        }
 
     #change this
     result = 'null'
