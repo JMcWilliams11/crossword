6ce9b2849060e7edd341fe43e0c7f21fa0c8e6fa
Jack McWilliams <jmcwill@umich.edu>
2016-10-08 12:19:00 -0400

    JM - fixed malformed URL bug

--- controllers/solve.py
+++ controllers/solve.py
@@ -1,6 +1,7 @@
 from flask import *
 import requests
 import bs4
+import validators
 
 solve = Blueprint('solve', __name__, template_folder='templates')
 
@@ -8,15 +9,20 @@ solve = Blueprint('solve', __name__, template_folder='templates')
 def solve_route():
     #puzzleUrl = new york times crossword puzzle url
     puzzleUrl = request.form['url']
-    req = requests.get(puzzleUrl)
+    if validators.url(puzzleUrl) :
+        req = requests.get(puzzleUrl)
+    else :
+        options = {
+            'goodUrl': False
+        }
+        return render_template("index.html", **options)
     if req.status_code == requests.codes.ok :
         puzzleSoup = bs4.BeautifulSoup(req.text)
         grid = puzzleSoup.select('.flex-row')
-        cluelist = puzzleSoup.select('.clue-list-wrapper')
+        clueLists = puzzleSoup.select('.clue-list-wrapper')
         options = {
             'result': grid
         }
-        print grid
 
     else :
         options = {
